f20ec5b6e7e3b31b5dbb58f0c24813633374f1ba
pascalstad <pascal.stadtmueller@googlemail.com>
2023-09-25 13:20:45 +0200

    added programm.py and test_programm.py

---
+++ programm.py
@@ -0,0 +1,8 @@
+def plusmal(x,y):
+    return (x+y) * 2
+
+def unterschreiben(x):
+    return x
+
+def kubieren(x):
+    return x * x * x
---
+++ test_programm.py
@@ -0,0 +1,13 @@
+from programm import *
+
+def test_plusmal():
+    assert plusmal(2,3) == 10
+
+def test_unterschreiben():
+    assert unterschreiben("test")
+
+def test_kubieren():
+    assert kubieren(2) == 8
+
+def test_kubieren1():
+    assert kubieren(3) == 27
